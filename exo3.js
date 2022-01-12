fcfbfbe28eaa324106585d73dfa803d08e5013d4
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-12 19:51:59 +0100

    solves with O(n)

---
+++ exo3.js
@@ -0,0 +1,18 @@
+// Avec une liste de nombres entiers relatifs et un nombre k, 
+// crée une méthode retournant un booléen qui affiche si deux  
+// nombres de cette liste ont k comme résultat de leur somme.
+
+const list1 = [10, 15, 3, 7];
+const k1 = 17;
+const list2 = [1, 8, 10, 21];
+const k2 = 19;
+
+const twoNumbersEnough = (array, number) => {
+  for(let i = 0; i < array.length; i++) {
+    const valueToFind = number - array[i]; // value to be find in the array in order to return true
+    return array.find(element => element === valueToFind) != undefined ? true : false;
+  }
+}
+
+console.log("Avec [10, 15, 3, 7] et k = 17, l'algo retourne : " + twoNumbersEnough(list1, k1));
+console.log("Avec [1, 8, 10, 21] et k = 19, l'algo retourne : " + twoNumbersEnough(list2, k2));
